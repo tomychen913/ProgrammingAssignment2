be233cd61f20aabc29fac24ebadb8a3804e0e354
tomychen913 <tony552355@gmail.com>
2015-07-23 00:53:06 +0800

    Caching the Inverse of a Matrix done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do OK
+## Caching the Inverse of a Matrix to reduce the time-consuming
+## computations effect.
 
-## Write a short comment describing this function
+
+## MakeCacheMatrix creates a special "matrix" object that can cache its inverse.
+## The special "matrix", which is a list containing a function to
+## 1.set the matrix
+## 2.get the matrix
+## 3.set the inverse of the matrix
+## 4.get the inverse of the matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    TempMatrix <- NULL
+    set <- function(y) {
+        ## Use the '<<-' operator to assign a value to an object in an 
+        ## environment different from the current environment.
+        x <<- y
+        TempMatrix <<- NULL
+    }
+    get <- function() x
+    setInver <- function(Inverse) TempMatrix <<- Inverse
+    getInver <- function() TempMatrix
+    list(set = set, get = get, setInver = setInver, getInver = getInver)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special "matrix" returned by  
+## makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then cacheSolve should retrieve the 
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Check inverse of the matrix is exist or not.
+    TempMatrix <- x$getInver()
+    ## If the value is not null, display the message and return the matrix that 
+    ## has been calculated.
+    if(!is.null(TempMatrix)) {
+        message("getting cached data")
+        return(TempMatrix)
+    }
+    ## Get the matrix
+    data <- x$get()
+    ## Use 'solve' to Computing the inverse of a square matrix.
+    TempMatrix <- solve(data, ...)
+    ## Save the Computed result
+    x$setInver(TempMatrix)
+    ## Return a matrix that is the inverse of 'x'
+    return(TempMatrix)  
 }
